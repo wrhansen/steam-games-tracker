cbed842e7615185ad615c0ee80c9b22c89a0f197
Wesley Hansen <wesrhansen@gmail.com>
2023-07-18 21:45:52 -0400

    fix signature

--- data_objects.py
+++ data_objects.py
@@ -34,7 +34,7 @@ class Game:
     has_leaderboards: bool = False
 
     @classmethod
-    def from_dict(cls, env):
+    def from_dict(cls, **env):
         return cls(
             **{k: v for k, v in env.items() if k in inspect.signature(cls).parameters}
         )
